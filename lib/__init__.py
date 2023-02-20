d2980a5025b39448bcfbf33ed18ca6363de1dfa7
sunshinekoo <350311204@qq.com>
2023-02-20 14:24:50 +0800

    Create __init__.py

---
+++ lib/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+"""
+* @ Elephant for Python
+##############################################################################
+# Author: YWJT / ZhiQiang Koo                                                #
+# Modify: 2019-11-06                                                         #
+##############################################################################
+# This program is distributed under the "Artistic License" Agreement         #
+# The LICENSE file is located in the same directory as this program. Please  #
+# read the LICENSE file before you make copies or distribute this program    #
+##############################################################################
+"""
+
+__VERSION__ = "0.2.4-beta"
